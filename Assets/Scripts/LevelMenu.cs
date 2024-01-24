51cde6d9cf62528b1b3191bbea882cc75287fa9b
eliahdim <eliah.vilding@hotmail.se>
2024-01-24 16:18:01 +0100

    Level menu funcionality

--- Assets/Scripts/LevelMenu.cs
+++ Assets/Scripts/LevelMenu.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OpenLevel(int LevelId)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        string levelName = "Level " + LevelId;
+        SceneManager.LoadScene(levelName);
     }
 }
